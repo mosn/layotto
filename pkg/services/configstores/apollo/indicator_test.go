2d1cdf025dd04467c0530a74064262fc3a475e97
seeflood <349895584@qq.com>
2021-05-18 15:18:05 +0800

    add indicator_test

---
+++ pkg/services/configstores/apollo/indicator_test.go
@@ -0,0 +1,25 @@
+package apollo
+
+import (
+	"github.com/layotto/layotto/pkg/filter/stream/actuator/health"
+	testify "github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetHealthInitOrSuccess(t *testing.T) {
+	assert := testify.New(t)
+
+	hi := newHealthIndicator()
+	h := hi.Report()
+	assert.Equal(h.Status, health.UP)
+}
+
+func TestGetHealthError(t *testing.T) {
+	assert := testify.New(t)
+
+	hi := newHealthIndicator()
+	hi.ReportError("sub error")
+	h := hi.Report()
+	assert.Equal(h.Status, health.DOWN)
+	assert.Equal(h.Details[reasonKey], "sub error")
+}
