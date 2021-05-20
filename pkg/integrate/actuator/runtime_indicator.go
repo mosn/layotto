e91b291d751707adb0e771bee21c71c7afa6ef4b
seeflood <349895584@qq.com>
2021-05-20 11:23:48 +0800

    fix bug

--- pkg/integrate/actuator/runtime_indicator.go
+++ pkg/integrate/actuator/runtime_indicator.go
@@ -39,7 +39,7 @@ func GetRuntimeReadinessIndicator() RuntimeIndicator {
 }
 
 func GetRuntimeLivenessIndicator() RuntimeIndicator {
-	return runtimeReady
+	return runtimeLive
 }
 
 type runtimeIndicatorImpl struct {
