a36a4511d6f2793ef2e89ced17da06ca1432bddb
seeflood <349895584@qq.com>
2021-06-16 10:46:07 +0800

    improve apollo demo (#52)

--- demo/configuration/apollo/client.go
+++ demo/configuration/apollo/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"google.golang.org/grpc"
 	client "mosn.io/layotto/sdk/go-sdk/client"
@@ -22,21 +23,73 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	testSet(cli)
-	// Since configuration data in apollo cache is eventual-consistent,we need to sleep a while before querying new data
-	time.Sleep(time.Second * 1)
-	testGet(cli)
+	defer cli.Close()
+	ctx := context.Background()
 
-	testDelete(cli)
+	// Belows are CRUD examples
+	// 1. set
+	testSet(ctx, cli)
+	// 2. get after set
+	// Since configuration data in apollo cache is eventual-consistent,we need to sleep a while before querying new data
+	time.Sleep(time.Second * 2)
+	testGet(ctx, cli)
+
+	// 3. delete
+	testDelete(ctx, cli)
+
+	// 4. get after delete
 	//sleep a while before querying deleted data
-	time.Sleep(time.Second * 1)
-	testGet(cli)
+	time.Sleep(time.Second * 2)
+	testGet(ctx, cli)
+
+	// 5. show how to use subscribe API
+	// with sdk
+	//testSubscribeWithSDK(ctx, cli)
+
 	// besides sdk,u can also call our server with grpc
-	testSubscribeWithGrpc()
-	cli.Close()
+	testSubscribeWithGrpc(ctx)
 }
 
-func testSubscribeWithGrpc() {
+func testSubscribeWithSDK(ctx context.Context, cli client.Client) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	// 1. subscribe
+	ch := cli.SubscribeConfiguration(ctx, &client.ConfigurationRequestItem{
+		StoreName: storeName, Group: "application", Label: "prod", AppId: appid, Keys: []string{"haha", "heihei"},
+	})
+	// 2. client loop receiving changes in another gorountine
+	go func() {
+		for resp := range ch {
+			if resp.Err != nil {
+				fmt.Println("subscribe failed", resp.Err)
+				continue
+			}
+			marshal, err := json.Marshal(resp.Item)
+			if err != nil {
+				fmt.Println("marshal resp.Item failed.")
+				return
+			}
+			fmt.Printf("receive subscribe resp: %+v\n", string(marshal))
+		}
+		fmt.Println("subscribe channel has been closed.")
+	}()
+
+	// 3. loop setting configuration in another gorountine
+	go func() {
+		idx := 0
+		for {
+			time.Sleep(1 * time.Second)
+			idx++
+			newItmes := make([]*client.ConfigurationItem, 0, 10)
+			newItmes = append(newItmes, &client.ConfigurationItem{Group: "application", Label: "prod", Key: "heihei", Content: "heihei" + strconv.Itoa(idx)})
+			fmt.Println("write start")
+			cli.SaveConfiguration(ctx, &client.SaveConfigurationRequest{StoreName: storeName, AppId: appid, Items: newItmes})
+		}
+	}()
+	wg.Wait()
+}
+
+func testSubscribeWithGrpc(ctx context.Context) {
 	// 1. connect with grpc
 	conn, err := grpc.Dial("127.0.0.1:34904", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(2*time.Second))
 	if err != nil {
@@ -44,8 +97,6 @@ func testSubscribeWithGrpc() {
 	}
 	defer conn.Close()
 	c := runtimev1pb.NewRuntimeClient(conn)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
 	// get client for subscribe
 	var wg sync.WaitGroup
@@ -84,7 +135,7 @@ func testSubscribeWithGrpc() {
 	wg.Wait()
 }
 
-func testSet(cli client.Client) {
+func testSet(ctx context.Context, cli client.Client) {
 	item1 := &client.ConfigurationItem{Group: group, Label: "prod", Key: "key1", Content: "value1", Tags: map[string]string{
 		"release": "1.0.0",
 		"feature": "print",
@@ -96,16 +147,16 @@ func testSet(cli client.Client) {
 	saveRequest := &client.SaveConfigurationRequest{StoreName: storeName, AppId: appid}
 	saveRequest.Items = append(saveRequest.Items, item1)
 	saveRequest.Items = append(saveRequest.Items, item2)
-	if cli.SaveConfiguration(context.Background(), saveRequest) != nil {
+	if cli.SaveConfiguration(ctx, saveRequest) != nil {
 		fmt.Println("save key failed")
 		return
 	}
 	fmt.Println("save key success")
 }
 
-func testGet(cli client.Client) {
+func testGet(ctx context.Context, cli client.Client) {
 	getRequest := &client.ConfigurationRequestItem{StoreName: storeName, AppId: appid, Group: group, Label: "prod", Keys: []string{"key1", "haha"}}
-	items, err := cli.GetConfiguration(context.Background(), getRequest)
+	items, err := cli.GetConfiguration(ctx, getRequest)
 	//validate
 	if err != nil {
 		fmt.Printf("get configuration failed %+v \n", err)
@@ -115,9 +166,9 @@ func testGet(cli client.Client) {
 	}
 }
 
-func testDelete(cli client.Client) {
+func testDelete(ctx context.Context, cli client.Client) {
 	request := &client.ConfigurationRequestItem{StoreName: storeName, AppId: appid, Group: group, Label: "prod", Keys: []string{"key1", "haha"}}
-	if cli.DeleteConfiguration(context.Background(), request) != nil {
+	if cli.DeleteConfiguration(ctx, request) != nil {
 		fmt.Println("delete key failed")
 		return
 	}
