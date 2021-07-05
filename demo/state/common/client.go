6b9476feb468f80b1ecb8701078bcb02a974a80c
seeflood <349895584@qq.com>
2021-07-05 17:43:27 +0800

    add developer guide for components

--- demo/state/common/client.go
+++ demo/state/common/client.go
@@ -18,20 +18,27 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	client "mosn.io/layotto/sdk/go-sdk/client"
 )
 
 const (
-	storeName = "redis"
-	key1      = "key1"
-	key2      = "key2"
-	key3      = "key3"
-	key4      = "key4"
-	key5      = "key5"
+	key1 = "key1"
+	key2 = "key2"
+	key3 = "key3"
+	key4 = "key4"
+	key5 = "key5"
 )
 
+var storeName string
+
+func init() {
+	flag.StringVar(&storeName, "s", "", "set `storeName`")
+}
+
 func main() {
+	flag.Parse()
 	cli, err := client.NewClient()
 	if err != nil {
 		panic(err)
