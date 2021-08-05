a4c77f03613dde6a83e8e66fc002715a38758a90
文徐 <wangwenxue.wwx@alibaba-inc.com>
2021-08-05 12:35:04 +0800

    update

--- components/rpc/invoker/mosn/channel/xchannel.go
+++ components/rpc/invoker/mosn/channel/xchannel.go
@@ -21,7 +21,6 @@ import (
 	"errors"
 	"fmt"
 	"net"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -155,7 +154,6 @@ func (m *xChannel) removeCall(xstate *xstate, id uint32) {
 
 func (m *xChannel) onData(conn *wrapConn) error {
 	xstate := conn.state.(*xstate)
-	StartTime := time.Now()
 	for {
 		var iframe interface{}
 		iframe, err := m.proto.Decode(context.TODO(), conn.buf)
@@ -184,8 +182,6 @@ func (m *xChannel) onData(conn *wrapConn) error {
 			notifyChan <- &call{resp: frame}
 		}
 	}
-	endtime := time.Now()
-	fmt.Printf("解码时间是：%+v \n", strconv.FormatInt(endtime.Sub(StartTime).Nanoseconds()/1000, 10))
 	return nil
 }
 
