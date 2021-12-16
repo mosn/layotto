e0663d1c8315394c86218f12f33f417e28fa45b7
seeflood <349895584@qq.com>
2021-12-16 21:32:50 +0800

    remove comment

--- etc/script/report.sh
+++ etc/script/report.sh
@@ -20,28 +20,3 @@ echo "test go-sdk"
 go test -count=1 -failfast -timeout 120s ./... -coverprofile cover.out -covermode=atomic
 cat cover.out >> ../../cover.out
 cd ../..
-
-#go tool cover -html=cover.out -o coverage.html
-#go tool cover -func=cover.out -o func.out
-#echo test func-coverage $(tail -1 func.out | awk '{print $3}')
-
-#for d in $(go list ./pkg/...); do
-#    echo "--------Run test package: $d"
-#    GO111MODULE=off go test -gcflags="all=-N -l" -v -coverprofile=profile.out -covermode=atomic $d
-#    echo "--------Finish test package: $d"
-#    if [ -f profile.out ]; then
-#        cat profile.out >> coverage.txt
-#        rm profile.out
-#    fi
-#done
-#
-#cd components
-#for d in $(go list ./...); do
-#    echo "--------Run test package: $d"
-#    GO111MODULE=off go test -gcflags=-l -v -coverprofile=profile.out -covermode=atomic $d
-#    echo "--------Finish test package: $d"
-#    if [ -f profile.out ]; then
-#	cat profile.out >> coverage.txt
-#	rm profile.out
-#    fi
-#done
