42f953c99b3467cb2343f80ddf017278decbaa2e
stulzq <stulzq@qq.com>
2022-02-25 15:42:03 +0800

    feat: add file alicloud ut

--- components/file/s3/alicloud/oss_test.go
+++ components/file/s3/alicloud/oss_test.go
@@ -18,6 +18,7 @@ package alicloud
 
 import (
 	"context"
+	"io"
 	"testing"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
@@ -27,14 +28,15 @@ import (
 	"mosn.io/layotto/components/file"
 )
 
-func TestInit(t *testing.T) {
-	data := `[
+const data = `[
 				{
 					"endpoint": "endpoint_address",
 					"accessKeyID": "accessKey",
 					"accessKeySecret": "secret"
 				}
 			]`
+
+func TestInit(t *testing.T) {
 	fc := file.FileConfig{}
 	oss := NewAliCloudOSS()
 	err := oss.Init(context.TODO(), &fc)
@@ -62,4 +64,172 @@ func TestSelectClient(t *testing.T) {
 	client, err = ossObject.selectClient()
 	assert.Equal(t, err.Error(), "should specific endpoint in metadata")
 	assert.Nil(t, client)
+}
+
+func TestGetBucket(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	ac := oss.(*AliCloudOSS)
+	mt := make(map[string]string)
+
+	bucket, err := ac.getBucket("/", mt)
+	assert.Equal(t, err.Error(), "invalid fileName format")
+	assert.Nil(t, bucket)
+
+	bucket, err = ac.getBucket("b/a.txt", mt)
+	assert.Equal(t, err.Error(), "bucket name b len is between [3-63],now is 1")
+	assert.Nil(t, bucket)
+
+	bucket, err = ac.getBucket("bbbb/a.txt", mt)
+	assert.NoError(t, err)
+	assert.Equal(t, bucket.BucketName, "bbbb")
+}
+
+func TestGetClient(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	ac := oss.(*AliCloudOSS)
+	mt := &OssMetadata{
+		Endpoint:        "endpoint",
+		AccessKeyID:     "ak",
+		AccessKeySecret: "ak",
+	}
+
+	//TODO test empty endpoint/ak/sk , now will get panic
+
+	client, err := ac.getClient(mt)
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+}
+
+func TestCheckMetadata(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	ac := oss.(*AliCloudOSS)
+	mt := &OssMetadata{
+		Endpoint:        "",
+		AccessKeyID:     "",
+		AccessKeySecret: "",
+	}
+
+	assert.False(t, ac.checkMetadata(mt))
+	mt.Endpoint = "e"
+	assert.False(t, ac.checkMetadata(mt))
+	mt.AccessKeySecret = "sk"
+	assert.False(t, ac.checkMetadata(mt))
+	mt.AccessKeyID = "ak"
+	assert.True(t, ac.checkMetadata(mt))
+}
+
+func TestPut(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	req := &file.PutFileStu{
+		FileName: "",
+	}
+	err = oss.Put(context.Background(), req)
+	assert.Error(t, err)
+
+	req.FileName = "b/a.txt"
+	err = oss.Put(context.Background(), req)
+	assert.Error(t, err)
+}
+
+func TestGet(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	req := &file.GetFileStu{
+		FileName: "",
+	}
+
+	var resp io.ReadCloser
+	resp, err = oss.Get(context.Background(), req)
+	assert.Error(t, err)
+	assert.Nil(t, resp)
+
+	req.FileName = "b/a.txt"
+	resp, err = oss.Get(context.Background(), req)
+	assert.Error(t, err)
+}
+
+func TestStat(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	req := &file.FileMetaRequest{
+		FileName: "",
+	}
+
+	var resp *file.FileMetaResp
+	resp, err = oss.Stat(context.Background(), req)
+	assert.Error(t, err)
+	assert.Nil(t, resp)
+
+	req.FileName = "b/a.txt"
+	resp, err = oss.Stat(context.Background(), req)
+	assert.Error(t, err)
+}
+
+func TestList(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	req := &file.ListRequest{
+		DirectoryName: "",
+		PageSize:      0,
+	}
+
+	var resp *file.ListResp
+	resp, err = oss.List(context.Background(), req)
+	assert.Error(t, err)
+	assert.Nil(t, resp)
+
+	req.DirectoryName = "b/"
+	resp, err = oss.List(context.Background(), req)
+	assert.Error(t, err)
+}
+
+func TestDel(t *testing.T) {
+	fc := file.FileConfig{}
+	oss := NewAliCloudOSS()
+	fc.Metadata = []byte(data)
+	err := oss.Init(context.TODO(), &fc)
+	assert.Nil(t, err)
+
+	req := &file.DelRequest{
+		FileName: "",
+	}
+
+	err = oss.Del(context.Background(), req)
+	assert.Error(t, err)
+
+	req.FileName = "b/a.txt"
+	err = oss.Del(context.Background(), req)
+	assert.Error(t, err)
 }
