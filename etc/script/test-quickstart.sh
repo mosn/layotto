a49cf71e30ec1199c5996603be9b90e87d2b0c01
seeflood <349895584@qq.com>
2022-04-09 18:56:26 +0800

    release resources before run tests

--- etc/script/test-quickstart.sh
+++ etc/script/test-quickstart.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash
 
-docs=(docs/en/start/state/start.md
- docs/zh/start/state/start.md
- docs/en/start/lock/start.md
- docs/zh/start/lock/start.md
- docs/zh/start/file/minio.md
- docs/en/start/api_plugin/helloworld.md
- docs/zh/start/api_plugin/helloworld.md
- docs/zh/start/actuator/start.md
+quickstarts=(docs/en/start/state/start.md
+  docs/zh/start/state/start.md
+  docs/en/start/lock/start.md
+  docs/zh/start/lock/start.md
+  docs/zh/start/file/minio.md
+  docs/en/start/api_plugin/helloworld.md
+  docs/zh/start/api_plugin/helloworld.md
+  docs/zh/start/actuator/start.md
   docs/en/start/actuator/start.md
   docs/zh/start/trace/trace.md
   docs/en/start/trace/trace.md
@@ -19,18 +18,22 @@ docs=(docs/en/start/state/start.md
 export projectpath=$(pwd)
 export project_path=$(pwd)
 
-for doc in "${docs[@]}" ; do
+# release all resources
+function release_resource() {
+  killall layotto
+  if [ $(docker ps -a -q | wc -l) -gt 0 ]; then
+    docker rm -f $(docker ps -a -q)
+  fi
+}
+
+release_resource
+
+for doc in "${quickstarts[@]}"; do
   echo "Start testing $doc......"
 
   #./mdsh.sh docs/en/start/state/start.md
   #./mdsh.sh docs/zh/start/state/start.md
   ./mdsh.sh $doc
 
-  # release all resources
-  killall layotto
-  if [ $(docker ps -a -q|wc -l) -gt 0 ]
-  then
-       docker rm -f $(docker ps -a -q)
-  fi
-
+  release_resource
 done
