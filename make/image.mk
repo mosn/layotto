862633d66a6ceba092dbe7c971e9f94f2fafe104
Xunzhuo <mixdeers@gmail.com>
2022-04-24 02:43:09 +0800

    fix: add check

--- make/image.mk
+++ make/image.mk
@@ -60,49 +60,6 @@ image.build.%: go.build.%
 	$(eval BUILD_SUFFIX := $(_DOCKER_BUILD_EXTRA_ARGS) --pull -t $(REGISTRY_PREFIX)/$(IMAGE).$(ARCH):$(VERSION) $(TMP_DIR)/$(IMAGE))
 	$(DOCKER) build --platform $(IMAGE_PLAT) $(BUILD_SUFFIX)
 
-APPS ?= faas integrate
-APP_PLATFORMS = linux_amd64 linux_arm64 
-
-.PHONY: app.image
-app.image: image.verify  $(addprefix app.image., $(addprefix $(IMAGE_PLAT)., $(APPS)))
-
-.PHONY: app.image.multiarch
-app.image.multiarch: image.verify  $(foreach p,$(APP_PLATFORMS),$(addprefix app.image., $(addprefix $(p)., $(APPS))))
-
-.PHONY: app.image.%
-app.image.%:
-	$(eval PLATFORM := $(word 1,$(subst ., ,$*)))
-	$(eval APP := $(word 2,$(subst ., ,$*)))
-	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
-	$(eval IMAGE_PLAT := $(subst _,/,$(PLATFORM)))
-	@echo "===========> Building docker image $(APP) $(VERSION) for $(IMAGE_PLAT)"
-	@mkdir -p $(TMP_DIR)/$(APP)
-	@cat $(ROOT_DIR)/docker/app/$(APP)/Dockerfile\
-		>$(TMP_DIR)/$(APP)/Dockerfile
-	$(eval BUILD_SUFFIX := $(_DOCKER_BUILD_EXTRA_ARGS) --pull -t $(REGISTRY_PREFIX)/$(APP)-$(ARCH):$(VERSION) $(TMP_DIR)/$(APP))
-	$(DOCKER) build --platform $(IMAGE_PLAT) $(BUILD_SUFFIX)
-
-.PHONY: wasm.image
-wasm.image: wasm
-	$(eval IMAGE := layotto)
-	$(eval IMAGE_PLAT := $(subst _,/,$(WASM_PLATFORM)))
-	$(eval ARCH := $(word 2,$(subst _, ,$(WASM_PLATFORM))))
-	@echo "===========> Building docker image $(IMAGE) $(VERSION) for $(IMAGE_PLAT)"
-	@mkdir -p $(TMP_DIR)/$(IMAGE)
-	@cat $(ROOT_DIR)/docker/$(IMAGE)/Dockerfile\
-		>$(TMP_DIR)/$(IMAGE)/Dockerfile
-	@cp $(OUTPUT_DIR)/$(IMAGE_PLAT)/layotto $(TMP_DIR)/$(IMAGE)/
-	$(eval BUILD_SUFFIX := $(_DOCKER_BUILD_EXTRA_ARGS) --pull -t $(REGISTRY_PREFIX)/$(IMAGE).wasm.$(ARCH):$(VERSION) $(TMP_DIR)/$(IMAGE))
-	$(DOCKER) buildx build --platform $(IMAGE_PLAT) $(BUILD_SUFFIX)
-
-.PHONY: wasm.image.push
-wasm.image.push:
-	$(eval IMAGE := layotto)
-	$(eval ARCH := $(word 2,$(subst _, ,$(WASM_PLATFORM))))
-	$(eval IMAGE_PLAT := $(subst _,/,$(WASM_PLATFORM)))
-	@echo "===========> Pushing image $(IMAGE) $(VERSION) to $(REGISTRY_PREFIX)"
-	$(DOCKER) push $(REGISTRY_PREFIX)/$(IMAGE).wasm.$(ARCH):$(VERSION)
-
 .PHONY: image.push
 image.push: image.verify $(addprefix image.push., $(addprefix $(IMAGE_PLAT)., $(IMAGES)))
 
@@ -118,23 +75,3 @@ image.push.%:
 	$(eval IMAGE_PLAT := $(subst _,/,$(PLATFORM)))
 	@echo "===========> Pushing image $(IMAGE) $(VERSION) to $(REGISTRY_PREFIX)"
 	$(DOCKER) push $(REGISTRY_PREFIX)/$(IMAGE)-$(ARCH):$(VERSION)
-
-.PHONY: integration.wasm
-integration.wasm: app.image.linux_amd64.faas
-	@echo "===========> Integration Test With WASM"
-	$(eval ACTION := sh ./wasm_test.sh)
-	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
-	$(eval BUILD_IMAGE := $(REGISTRY_PREFIX)/faas-$(ARCH):$(VERSION))
-	$(eval WORKDIR := -w /go/src/${PROJECT_NAME} )
-	$(eval INTEGRATE_SUFFIX := -v $(ROOT_DIR):/go/src/${PROJECT_NAME} -v ${TEST_DIR}/wasm/wasm_test.sh:/go/src/${PROJECT_NAME}/wasm_test.sh $(WORKDIR))
-	$(DOCKER) run --rm $(INTEGRATE_SUFFIX) $(BUILD_IMAGE) $(ACTION)
-
-.PHONY: integration.runtime
-integration.runtime: app.image.linux_amd64.integrate
-	@echo "===========> Integration Test With Runtime"
-	$(eval ACTION := sh ./integrate_test.sh)
-	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
-	$(eval BUILD_IMAGE := $(REGISTRY_PREFIX)/integrate-$(ARCH):$(VERSION))
-	$(eval WORKDIR := -w /go/src/${PROJECT_NAME} )
-	$(eval INTEGRATE_SUFFIX := -v $(ROOT_DIR):/go/src/${PROJECT_NAME} -v ${TEST_DIR}/runtime/integrate_test.sh:/go/src/${PROJECT_NAME}/integrate_test.sh $(WORKDIR))
-	$(DOCKER) run --rm $(INTEGRATE_SUFFIX) ${BUILD_IMAGE} $(ACTION)
