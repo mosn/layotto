9b61c76d1975bf6b38a23bb66ee668d39f16f95b
Xunzhuo <mixdeers@gmail.com>
2022-04-23 23:23:48 +0800

    fix: add check

--- make/golang.mk
+++ make/golang.mk
@@ -31,7 +31,7 @@ go.build.%:
 	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
 	@echo "===========> Building binary $(COMMAND) $(VERSION) for $(OS) $(ARCH)"
 	@mkdir -p $(OUTPUT_DIR)/$(OS)/$(ARCH)
-	@CGO_ENABLED=0 GOOS=$(OS) GOARCH=$(ARCH) $(GO) build -o $(OUTPUT_DIR)/$(OS)/$(ARCH)/$(COMMAND)$(GO_OUT_EXT) -ldflags "$(GO_LDFLAGS)" $(ROOT_PACKAGE)/cmd/$(COMMAND)
+	@CGO_ENABLED=0 GOOS=$(OS) GOARCH=$(ARCH) $(GO) build -o $(OUTPUT_DIR)/$(OS)/$(ARCH)/$(COMMAND) -ldflags "$(GO_LDFLAGS)" $(ROOT_PACKAGE)/cmd/$(COMMAND)
 
 .PHONY: go.build
 go.build:  $(addprefix go.build., $(addprefix $(PLATFORM)., $(BINS)))
@@ -39,28 +39,34 @@ go.build:  $(addprefix go.build., $(addprefix $(PLATFORM)., $(BINS)))
 .PHONY: go.build.multiarch
 go.build.multiarch:  $(foreach p,$(PLATFORMS),$(addprefix go.build., $(addprefix $(p)., $(BINS))))
 
-.PHONY: build.wasm
-build.wasm:  $(addprefix build.wasm., $(addprefix $(PLATFORM)., $(BINS)))
+WASM_PLATFORM ?= linux_amd64
+WASM_PLATFORMS ?= linux_amd64
+WASM_BUILD ?= faas
 
-.PHONY: build.wasm.multiarch
-build.wasm.multiarch:  $(foreach p,$(PLATFORMS),$(addprefix build.wasm., $(addprefix $(p)., $(BINS))))
+.PHONY: wasm
+wasm:  $(addprefix wasm., $(addprefix $(WASM_PLATFORM)., $(WASM_BUILD)))
 
-.PHONY: build.wasm.%
-build.wasm.%:
+.PHONY: wasm.multiarch
+wasm.multiarch:  $(foreach p,$(WASM_PLATFORMS),$(addprefix wasm., $(addprefix $(p)., $(WASM_BUILD))))
+
+.PHONY: wasm.%
+wasm.%:
 	$(eval COMMAND := $(word 2,$(subst ., ,$*)))
 	$(eval PLATFORM := $(word 1,$(subst ., ,$*)))
 	$(eval OS := $(word 1,$(subst _, ,$(PLATFORM))))
 	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
-	@echo "===========> Building wasm base image $(COMMAND) $(VERSION) for $(OS) $(ARCH)"
-	@make app.image.$(PLATFORM).faas
-	@echo "===========> Building binary wasm $(COMMAND) $(VERSION) for $(OS) $(ARCH)"
-	@mkdir -p $(OUTPUT_DIR)/$(OS)/$(ARCH)
 	$(eval BUILD_IMAGE := $(REGISTRY_PREFIX)/faas-$(ARCH):$(VERSION))
-	$(eval WORKDIR := -w /go/src/${PROJECT_NAME})
-	$(eval ENV := -e CGO_ENABLED=0 -e GOOS=$(OS) -e GOARCH=$(ARCH))
-	$(eval INTEGRATE_SUFFIX := -v $(ROOT_DIR):/go/src/${PROJECT_NAME} $(WORKDIR) $(ENV))
-	$(eval ACTION := $(GO) build -o $(OUTPUT_DIR)/$(OS)/$(ARCH)/$(COMMAND)$(GO_OUT_EXT) -tags wasmer -ldflags "$(GO_LDFLAGS)" $(ROOT_PACKAGE)/cmd/$(COMMAND))
-	$(DOCKER) run --rm $(INTEGRATE_SUFFIX) $(BUILD_IMAGE) $(ACTION)
+	@mkdir -p $(TMP_DIR)/$(COMMAND)
+	@mkdir -p $(OUTPUT_DIR)/$(OS)/$(ARCH)
+	@cat $(ROOT_DIR)/docker/app/$(COMMAND)/Dockerfile\
+		>$(TMP_DIR)/$(COMMAND)/Dockerfile
+	$(eval DOCKER_FILE := $(TMP_DIR)/$(COMMAND)/Dockerfile)
+	@echo "===========> Building wasm base image in $(VERSION) for $(OS) $(ARCH)"
+	$(DOCKER) build -f ${DOCKER_FILE} -t  ${BUILD_IMAGE} .
+	@echo "===========> Building binary wasm in $(VERSION) for $(OS) $(ARCH)"
+	$(eval OUTPUT_PATH := ./_output/$(OS)/$(ARCH)/$(COMMAND))
+	$(eval ACTION := $(GO) build -o $(OUTPUT_PATH) -tags wasmer -ldflags "$(GO_LDFLAGS)" $(ROOT_PACKAGE)/cmd/layotto)
+	$(DOCKER) run --rm -v $(ROOT_DIR):/go/src/${PROJECT_NAME} -w /go/src/${PROJECT_NAME} ${BUILD_IMAGE} ${ACTION}
 
 .PHONY: go.clean
 go.clean:
@@ -87,15 +93,16 @@ ifeq ($(shell which go-junit-report), )
 	@GO111MODULE=off $(GO) get -u github.com/jstemmer/go-junit-report
 endif
 
-.PHONY: go.style.verify
-go.style.verify:  
-	@echo "===========> Running go style check"
-	$(GO) fmt ./... && git status && [[ -z `git status -s` ]]
 
 .PHONY: go.test
 go.test: go.test.verify
 	@echo "===========> Run unit test"
 	$(GO) test -count=1 -timeout=10m -short -v `go list ./...|grep -v mosn.io/layotto/test` 2>&1 | tee >(go-junit-report --set-exit-code >$(OUTPUT_DIR)/report.xml)
+
+.PHONY: go.style
+go.style:  
+	@echo "===========> Running go style check"
+	$(GO) fmt ./... && git status && [[ -z `git status -s` ]]
 
 .PHONY: checker.deadlink
 checker.deadlink:
