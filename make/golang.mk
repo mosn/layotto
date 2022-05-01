b87639220a112de9bc797c4db28286f32aa9ac9b
Xunzhuo <mixdeers@gmail.com>
2022-05-02 04:57:37 +0800

    fix: add version to build flags
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- make/golang.mk
+++ make/golang.mk
@@ -20,6 +20,7 @@ GO_LDFLAGS += -X $(VERSION_PACKAGE).GitVersion=$(VERSION) \
 	-X $(VERSION_PACKAGE).GitCommit=$(GIT_COMMIT) \
 	-X $(VERSION_PACKAGE).GitTreeState=$(GIT_TREE_STATE) \
 	-X $(VERSION_PACKAGE).BuildDate=$(shell date -u +'%Y-%m-%dT%H:%M:%SZ') \
+	-X main.Version=$(VERSION) \
 
 ifeq ($(ROOT_PACKAGE),)
 	$(error the variable ROOT_PACKAGE must be set prior to including golang.mk)
