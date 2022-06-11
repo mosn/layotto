0435c3de2f411083c2c71a5d5b78b361b2c209ac
Azhs <zzhncut123@163.com>
2022-06-11 13:57:59 +0800

    sequencer ID

--- components/sequencer/postgresql/postgresql_sequencer_test.go
+++ components/sequencer/postgresql/postgresql_sequencer_test.go
@@ -15,7 +15,6 @@
 package postgresql
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -124,34 +123,34 @@ func TestPostgresqlSequencer_GetSegment(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestLocalNextId(t *testing.T) {
-
-	//db := utils.NewPostgresqlCli(params)
-	p := NewPostgresqlSequencer(log.DefaultLogger)
-	cfg := sequencer.Configuration{
-		Properties: initMap(),
-		BiggerThan: make(map[string]int64),
-	}
-	err := p.Init(cfg)
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < 10; i++ {
-		resp, err := p.GetNextId(&sequencer.GetNextIdRequest{Key: p.metadata.BizTag, Options: sequencer.SequencerOptions{AutoIncrement: sequencer.STRONG}, Metadata: nil})
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("next id : ", resp.NextId)
-	}
-
-	//ctx, _ := context.WithCancel(context.Background())
-	////
-	//updateParams := fmt.Sprintf(`update %v set value_id = value_id + 1, update_time = $1 where biz_tag = $2`, "layotto_incr")
-	//_, err = db.ExecContext(ctx, updateParams, time.Now().Unix(), "test")
-	//if err != nil {
-	//	fmt.Println("err: ", err)
-	//}
-}
+//func TestLocalNextId(t *testing.T) {
+//
+//	//db := utils.NewPostgresqlCli(params)
+//	p := NewPostgresqlSequencer(log.DefaultLogger)
+//	cfg := sequencer.Configuration{
+//		Properties: initMap(),
+//		BiggerThan: make(map[string]int64),
+//	}
+//	err := p.Init(cfg)
+//	if err != nil {
+//		panic(err)
+//	}
+//	for i := 0; i < 10; i++ {
+//		resp, err := p.GetNextId(&sequencer.GetNextIdRequest{Key: p.metadata.BizTag, Options: sequencer.SequencerOptions{AutoIncrement: sequencer.STRONG}, Metadata: nil})
+//		if err != nil {
+//			panic(err)
+//		}
+//		fmt.Println("next id : ", resp.NextId)
+//	}
+//
+//	//ctx, _ := context.WithCancel(context.Background())
+//	////
+//	//updateParams := fmt.Sprintf(`update %v set value_id = value_id + 1, update_time = $1 where biz_tag = $2`, "layotto_incr")
+//	//_, err = db.ExecContext(ctx, updateParams, time.Now().Unix(), "test")
+//	//if err != nil {
+//	//	fmt.Println("err: ", err)
+//	//}
+//}
 
 func TestInsert(t *testing.T) {
 	//
