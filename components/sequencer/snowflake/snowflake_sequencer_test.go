fa0e59a04bc0050894eef3a703a76604cb40c5aa
lh <321642771@qq.com>
2022-09-05 18:59:16 +0800

    add UT

--- components/sequencer/snowflake/snowflake_sequencer_test.go
+++ components/sequencer/snowflake/snowflake_sequencer_test.go
@@ -15,7 +15,8 @@ package snowflake
 
 import (
 	"database/sql"
-	"fmt"
+	"runtime"
+	"sync"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -87,15 +88,76 @@ func TestSnowFlakeSequence_GetNextId(t *testing.T) {
 
 	mset := mapset.NewSet()
 
-	var size int = 1000
+	var size int = 10000
 	for i := 0; i < size; i++ {
 		resp, err := s.GetNextId(&sequencer.GetNextIdRequest{
 			Key: key,
 		})
-
-		fmt.Printf("%b \n", resp.NextId)
 		assert.NoError(t, err)
 		mset.Add(resp.NextId)
 	}
 	assert.Equal(t, size, mset.Cardinality())
+}
+
+func TestSnowFlakeSequence_ParallelGetNextId(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	s := NewSnowFlakeSequencer(log.DefaultLogger)
+	s.db = db
+
+	mock.ExpectQuery("SELECT TABLE_NAME").WillReturnError(sql.ErrNoRows)
+	mock.ExpectExec("CREATE TABLE").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT HOST_NAME").WillReturnError(sql.ErrNoRows)
+	mock.ExpectExec("INSERT INTO").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectQuery("SELECT ID").WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectCommit()
+
+	cfg := sequencer.Configuration{
+		Properties: make(map[string]string),
+		BiggerThan: make(map[string]int64),
+	}
+
+	cfg.Properties["mysql"] = mysqlUrl
+	cfg.Properties["databaseName"] = databaseName
+	cfg.Properties["tableName"] = tableName
+	cfg.Properties["userName"] = userName
+	cfg.Properties["password"] = password
+
+	cfg.Properties["boostPower"] = boostPower
+	cfg.Properties["paddingFactor"] = paddingFactor
+	cfg.Properties["timeBits"] = timeBits
+	cfg.Properties["workerBits"] = workerBits
+	cfg.Properties["seqBits"] = seqBits
+	cfg.Properties["startTime"] = startTime
+
+	err = s.Init(cfg)
+
+	assert.NoError(t, err)
+
+	mset := mapset.NewSet()
+
+	var size int = 10000
+	var cores int = runtime.NumCPU()
+
+	var wg sync.WaitGroup
+	wg.Add(cores)
+
+	for i := 0; i < cores; i++ {
+		go func() {
+			for j := 0; j < size; j++ {
+				resp, err := s.GetNextId(&sequencer.GetNextIdRequest{
+					Key: key,
+				})
+				assert.NoError(t, err)
+				mset.Add(resp.NextId)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	assert.Equal(t, size * cores, mset.Cardinality())
 }
