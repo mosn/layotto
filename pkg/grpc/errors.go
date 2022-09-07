139e70ef6729c285fb85861c55ef6d8edab21275
seeflood <zhou.qunli@foxmail.com>
2022-09-07 15:44:57 +0800

    modify api
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- pkg/grpc/errors.go
+++ pkg/grpc/errors.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package lifecycle
+package grpc
 
 const (
 	ErrNoField           = "field %s is not configured"
