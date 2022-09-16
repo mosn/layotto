7ffeff5882c4d1706e2c46d1ef8daf7a048af707
Rayowang <rayo.wangzl@gmail.com>
2022-09-17 02:43:05 +0800

    fix define wasi, it is funny

--- pkg/wasm/runtime/wasmtime/function.go
+++ pkg/wasm/runtime/wasmtime/function.go
@@ -3,11 +3,11 @@ package wasmtime
 import 	wasmtimego "github.com/bytecodealliance/wasmtime-go"
 
 type Function struct {
-	vm           *VM
+	ins           *Instance
 	function  *wasmtimego.Func
 }
 
 func (self *Function) Call(parameters ...interface{}) (interface{}, error) {
-	return self.function.Call(self.vm.store, parameters...)
+	return self.function.Call(self.ins.store, parameters...)
 }
 
