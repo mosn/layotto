ae12e265c5a4d135b6bea6f878f613e38becb951
helbing <helbingxxx@gmail.com>
2022-09-26 15:56:32 +0800

    test: 💍 add integration tests for redis state

--- go.mod
+++ go.mod
@@ -4,15 +4,11 @@ go 1.14
 
 require (
 	github.com/99designs/keyring v1.2.1 // indirect
-	github.com/Azure/go-autorest/autorest/azure/cli v0.4.2 // indirect
-	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
-	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/SkyAPM/go2sky v0.5.0
 	github.com/agrea/ptr v0.0.0-20180711073057-77a518d99b7b
 	github.com/alicebob/miniredis/v2 v2.16.0
 	github.com/dapr/components-contrib v1.5.1-rc.1
 	github.com/dapr/kit v0.0.2-0.20210614175626-b9074b64d233
-	github.com/dimchansky/utfbom v1.1.1 // indirect
 	github.com/envoyproxy/go-control-plane v0.10.0
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gammazero/workerpool v1.1.2
@@ -21,15 +17,16 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/helbing/testtools v0.0.0-20220926001938-2b28af4a440f
 	github.com/json-iterator/go v1.1.12
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/openzipkin/zipkin-go v0.2.2
 	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
 	github.com/pkg/errors v0.9.1
 	github.com/shirou/gopsutil v3.21.3+incompatible
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.0
 	github.com/uber/jaeger-client-go v2.25.0+incompatible
-	github.com/urfave/cli v1.22.1
+	github.com/urfave/cli v1.22.2
 	github.com/valyala/fasthttp v1.28.0
 	go.uber.org/automaxprocs v1.4.0 // indirect
 	golang.org/x/tools v0.1.10 // indirect
@@ -38,6 +35,7 @@ require (
 	google.golang.org/protobuf v1.27.1
 	mosn.io/api v1.1.0
 	mosn.io/layotto/components v0.0.0-20220413092851-55c58dbb1a23
+	mosn.io/layotto/sdk/go-sdk v0.0.0-20220923091808-8cfe1169e23e
 	mosn.io/layotto/spec v0.0.0-20220413092851-55c58dbb1a23
 	mosn.io/mosn v1.1.0
 	mosn.io/pkg v1.1.0
---
+++ tests/integration/components/state/redis/redis_test.go
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2022 Layotto Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package redis
+
+import (
+	"context"
+	"testing"
+
+	"github.com/helbing/testtools/envs/dockercompose"
+	"github.com/helbing/testtools/flow"
+	"github.com/stretchr/testify/assert"
+
+	"mosn.io/layotto/sdk/go-sdk/client"
+)
+
+func TestComponentsStateRedis(t *testing.T) {
+	cli, err := client.NewClient()
+	assert.Nil(t, err)
+	defer cli.Close()
+
+	var (
+		storeName = "test_components_state_redis"
+		ctx       = context.Background()
+		value     = []byte("hello world")
+	)
+
+	flow.New(dockercompose.New("./docker-compose.yaml")).
+		Case("Test state", func(t *testing.T) {
+			key := "test_state_key"
+
+			// save
+			err = cli.SaveState(ctx, storeName, key, value)
+			assert.Nil(t, err)
+
+			// get
+			item, err := cli.GetState(ctx, storeName, key)
+			assert.Nil(t, err)
+			assert.Equal(t, key, item.Key)
+			assert.Equal(t, value, item.Value)
+
+			// delete
+			err = cli.DeleteStateWithETag(ctx, storeName, key, &client.ETag{
+				Value: item.Etag,
+			}, nil, nil)
+			assert.Nil(t, err)
+
+			// check deleted
+			item, err = cli.GetState(ctx, storeName, key)
+			assert.Nil(t, err)
+			assert.Equal(t, key, item.Key)
+			assert.Equal(t, []byte(nil), item.Value)
+		}).
+		Case("Test bulkstate", func(t *testing.T) {
+			key := "test_bulkstate_key"
+
+			item := &client.SetStateItem{
+				Key:   key,
+				Value: value,
+			}
+
+			// save
+			err = cli.SaveBulkState(ctx, storeName, item)
+			assert.Nil(t, err)
+
+			// get
+			items, err := cli.GetBulkState(ctx, storeName, []string{key}, nil, 3)
+			assert.Nil(t, err)
+			assert.Len(t, items, 1)
+			assert.Equal(t, key, items[0].Key)
+			assert.Equal(t, value, items[0].Value)
+		}).
+		Run(t)
+}
