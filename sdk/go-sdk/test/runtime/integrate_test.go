ad42ff947ca4165959d7dcdd83d51471d00e653b
seeflood <zhou.qunli@foxmail.com>
2022-10-19 17:17:10 +0800

    fix error
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- sdk/go-sdk/test/runtime/integrate_test.go
+++ sdk/go-sdk/test/runtime/integrate_test.go
@@ -53,7 +53,7 @@ func TestHelloApi(t *testing.T) {
 		ServiceName: "quick_start_demo",
 	}
 	helloResp, err := cli.SayHello(ctx, helloReq)
-	assert.NotNil(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "greeting", helloResp.Hello)
 }
 
