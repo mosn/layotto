3492043a709ce6acb07c37d4664b943e07727815
Rayowang <rayo.wangzl@gmail.com>
2022-11-05 12:48:36 +0800

    delete annotation

--- pkg/wasm/runtime/wasmtime/vm.go
+++ pkg/wasm/runtime/wasmtime/vm.go
@@ -1,3 +1,4 @@
+//go:build wasmer
 // +build wasmer
 
 // Copyright 2021 Layotto Authors
@@ -57,62 +58,3 @@ func (w *VM) NewModule(wasmBytes []byte) types.WasmModule {
 
 	return NewwasmtimegoModule(w, m, wasmBytes)
 }
-
-/*
-
-config := wasmtimego.NewConfig()
-	config.SetConsumeFuel(true)
-	engine := wasmtimego.NewEngineWithConfig(config)
-	store := wasmtimego.NewStore(engine)
-	err := store.AddFuel(10)
-	if err != nil {
-		panic(err)
-	}
-
-	wasm, err := wasmtimego.Wat2Wasm(`
-	(module
-         (func $addition (param i32 i32) (result i32)
-             get_local 0
-             get_local 1
-             i32.add
-         )
-         (func (export "arithmetic") (param $n i32) ( param $m i32) (result i32)
-             get_local $n
-             get_local $m
-             call $addition
-             i32.const 3
-             i32.mul
-         )
-    )`)
-
-	if err != nil {
-		panic(err)
-	}
-	module, err := wasmtimego.NewModule(store.Engine, wasm)
-	if err != nil {
-		panic(err)
-	}
-	instance, err := wasmtimego.NewInstance(store, module, []wasmtimego.AsExtern{})
-	if err != nil {
-		panic(err)
-	}
-
-	arithmetic := instance.GetFunc(store, "arithmetic")
-	if arithmetic == nil {
-		panic("failed to find function `arithmetic`")
-	}
-	for n := 0; n < 10; n++ {
-		fuelBefore, _ := store.FuelConsumed()
-		output, err := arithmetic.Call(store, n, n+1)
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		fuelAfter, _ := store.FuelConsumed()
-		fmt.Println(fmt.Sprintf("arithmetic(%d, %d) = %d [consumed %d fuel]\n", n, n+1, output, fuelAfter-fuelBefore))
-		err = store.AddFuel(fuelAfter - fuelBefore)
-		if err != nil {
-			panic(err)
-		}
-	}
-*/
