482acf8fa3cc4c8adf02226746ce60bb68d40ca1
seeflood <zhou.qunli@foxmail.com>
2023-03-16 22:58:26 +0800

    generate ut using Cursor
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- pkg/wasm/dispatch_test.go
+++ pkg/wasm/dispatch_test.go
@@ -2,6 +2,47 @@ package wasm
 
 import "testing"
 
+// Generated using Cursor
+// TestRouter tests the Router struct methods
+func TestRouter(t *testing.T) {
+	router := &Router{
+		routes: make(map[string]*Group),
+	}
+
+	plugin1 := &WasmPlugin{pluginName: "plugin1"}
+	plugin2 := &WasmPlugin{pluginName: "plugin2"}
+
+	// Test RegisterRoute
+	router.RegisterRoute("route1", plugin1)
+	router.RegisterRoute("route1", plugin2)
+
+	if len(router.routes["route1"].plugins) != 2 {
+		t.Errorf("Expected 2 plugins in route1, got %d", len(router.routes["route1"].plugins))
+	}
+
+	// Test RemoveRoute
+	router.RemoveRoute("route1")
+
+	if _, found := router.routes["route1"]; found {
+		t.Error("Expected route1 to be removed")
+	}
+
+	// Test GetRandomPluginByID
+	router.RegisterRoute("route2", plugin1)
+	router.RegisterRoute("route2", plugin2)
+
+	_, err := router.GetRandomPluginByID("route2")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	_, err = router.GetRandomPluginByID("nonexistent")
+	if err == nil {
+		t.Error("Expected error for nonexistent route")
+	}
+}
+
+// Generated using chatGPT
 func TestRouter_RegisterRoute(t *testing.T) {
 	// 1. Test case for appending a new plugin to an existing group when group is already present in route:
 	routes := make(map[string]*Group)
