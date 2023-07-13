c85bddeb5cc428765abb37587af114a2ddf4032c
文徐 <wangwenxue.wwx@alibaba-inc.com>
2023-07-13 14:23:59 +0800

    fix client and add recover for s3 interface

--- demo/cryption/client.go
+++ demo/cryption/client.go
@@ -30,43 +30,35 @@ const (
 )
 
 func TestEncrypt() []byte {
-	// Connect to the object store.
 	conn, err := grpc.Dial("127.0.0.1:34904", grpc.WithInsecure())
 	if err != nil {
 		fmt.Printf("failed to establish connection: %+v", err)
 		return nil
 	}
 
-	// Create a client for the object store.
 	c := cryption.NewCryptionServiceClient(conn)
 
-	// Create a request to get an object from the specified bucket.
 	req := &cryption.EncryptRequest{ComponentName: storeName, PlainText: []byte("Hello, world")}
 
-	// Retrieve the object using the client and the request.
 	resp, err := c.Encrypt(context.Background(), req)
 	if err != nil {
-		fmt.Printf("failed to retrieve object: %+v", err)
+		fmt.Printf("failed to Encrypt data: %+v", err)
 		return nil
 	}
 	return resp.CipherText
 }
 
 func TestDecrypt(data []byte) []byte {
-	// Connect to the object store.
 	conn, err := grpc.Dial("127.0.0.1:34904", grpc.WithInsecure())
 	if err != nil {
-		fmt.Printf("failed to establish connection: %+v", err)
+		fmt.Printf("failed to Decrypt data: %+v", err)
 		return nil
 	}
 
-	// Create a client for the object store.
 	c := cryption.NewCryptionServiceClient(conn)
 
-	// Create a request to get an object from the specified bucket.
 	req := &cryption.DecryptRequest{ComponentName: storeName, CipherText: data}
 
-	// Retrieve the object using the client and the request.
 	resp, err := c.Decrypt(context.Background(), req)
 	if err != nil {
 		fmt.Printf("failed to Decrypt: %+v", err)
